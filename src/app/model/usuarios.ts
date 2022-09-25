fc0015b495017d88de520fc05085db0ad36b87a3
unknown <giovanerr10@gmail.com>
2022-09-25 07:36:21 -0300

    interface user

--- src/app/model/usuarios.ts
+++ src/app/model/usuarios.ts
@@ -1,2 +1,8 @@
-export interface Service {
+export interface Usuarios {
+    nome: string, 
+    senha: string,
+    dataNascimento: Date,
+    endereco: string,
+    foto: string, 
+    genero: string
 }
